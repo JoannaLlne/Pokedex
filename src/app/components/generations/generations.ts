1b6221d0e49132ed03b676ca0e491afb8b435628
Joanna Lalanne <lalannejoanna@gmail.com>
2026-07-03 11:24:18 +0000

    Ajouts des composants Pokemons et Generations

--- src/app/components/generations/generations.ts
+++ src/app/components/generations/generations.ts
@@ -2,8 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-generations',
+  standalone: true,
   imports: [],
   templateUrl: './generations.html',
   styleUrl: './generations.css',
 })
-export class Generations {}
+export class Generations {
+  public generations : Array<string>
+
+  constructor() {
+    this.generations = []
+
+    this.generations.push("Génération I")
+    this.generations.push("Génération II")
+    this.generations.push("Génération III")
+    this.generations.push("Génération IV")
+    this.generations.push("Génération V")
+    this.generations.push("Génération VI")
+    this.generations.push("Génération VII")
+  }
+}
--- src/app/components/pokemons/pokemons.ts
+++ src/app/components/pokemons/pokemons.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { Pokemon } from '../models/pokemon';
 
 @Component({
   selector: 'app-pokemons',
+  standalone: true,
   imports: [],
   templateUrl: './pokemons.html',
   styleUrl: './pokemons.css',
 })
-export class Pokemons {}
+export class Pokemons {
+
+  public pokemons : Array<Pokemon>;
+
+  constructor() {
+    this.pokemons = []
+    this.pokemons.push({id: 1, name : "Bulbizarre", type: ["Plante, Poison"], height: 70, weigth: 690})
+    this.pokemons.push({id: 2, name : "Herbizarre", type: ["Plante, Poison"], height: 100, weigth: 1300})
+    this.pokemons.push({id: 3, name : "Florizarre", type: ["Plante, Poison"], height: 200, weigth: 1600})
+    this.pokemons.push({id: 4, name : "Salameche", type: ["Feu"], height: 60, weigth: 850})
+    this.pokemons.push({id: 5, name : "Reptincel", type: ["Feu"], height: 110, weigth: 1900})
+    this.pokemons.push({id: 6, name : "Dracaufeu", type: ["Feu, Vol"], height: 170, weigth: 9050})
+  }
+}
