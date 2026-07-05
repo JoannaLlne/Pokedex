06ccd16f4db49d3441150c94e418b80ca0d48d52
Joanna Lalanne <lalannejoanna@gmail.com>
2026-07-05 08:50:49 +0000

    Création service

--- src/app/components/pokemons/pokemons.ts
+++ src/app/components/pokemons/pokemons.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { Pokemon } from '../models/pokemon';
+import { PokemonService } from '../../../services/pokemon-service';
 
 @Component({
   selector: 'app-pokemons',
@@ -12,13 +13,13 @@ export class Pokemons {
 
   public pokemons : Array<Pokemon>;
 
-  constructor() {
+  constructor( @Inject(PokemonService) private pokemonService: PokemonService) {
     this.pokemons = []
-    this.pokemons.push({id: 1, name : "Bulbizarre", type: ["Plante, Poison"], height: 70, weigth: 690})
-    this.pokemons.push({id: 2, name : "Herbizarre", type: ["Plante, Poison"], height: 100, weigth: 1300})
-    this.pokemons.push({id: 3, name : "Florizarre", type: ["Plante, Poison"], height: 200, weigth: 1600})
-    this.pokemons.push({id: 4, name : "Salameche", type: ["Feu"], height: 60, weigth: 850})
-    this.pokemons.push({id: 5, name : "Reptincel", type: ["Feu"], height: 110, weigth: 1900})
-    this.pokemons.push({id: 6, name : "Dracaufeu", type: ["Feu, Vol"], height: 170, weigth: 9050})
+   this.getPokemons()
+  }
+
+  getPokemons() {
+    // Récupérer l'ensemble des pokemons
+    this.pokemons = this.pokemonService.getAllPokemons()
   }
 }
---
+++ src/services/pokemon-service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Pokemon } from '../app/components/models/pokemon';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class PokemonService {
+    constructor() {
+    }
+        getAllPokemons() {
+            let pokemons : Array<Pokemon> = []
+
+    pokemons.push({id: 1, name : "Bulbizarre", type: ["Plante, Poison"], height: 70, weight: 690})
+    pokemons.push({id: 2, name : "Herbizarre", type: ["Plante, Poison"], height: 100, weight: 1300})
+    pokemons.push({id: 3, name : "Florizarre", type: ["Plante, Poison"], height: 200, weight: 1600})
+    pokemons.push({id: 4, name : "Salameche", type: ["Feu"], height: 60, weight: 850})
+    pokemons.push({id: 5, name : "Reptincel", type: ["Feu"], height: 110, weight: 1900})
+    pokemons.push({id: 6, name : "Dracaufeu", type: ["Feu, Vol"], height: 170, weight: 9050})
+
+    return pokemons
+        }
+    }
+
+
